99b962e7494e73534c95b2b01ea5451a24c006af
Ike Elvis Ndubuisi <provictor.ie@gmail.com>
2024-07-16 09:12:03 +0100

    cors deployment fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://lbs-field.vercel.app"],
+    origin: "https://lbs-field.vercel.app",
     credentials: true,
     allowedHeaders: [
       "Access-Control-Allow-Origin",
